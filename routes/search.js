7af53f021d3f7de20a188e754110b966354b7cf2
Ahmed Abdalla <aabdalla@gmail.com>
2021-02-28 12:47:02 -0500

    apply callbacks for handling async db calls

--- routes/search.js
+++ routes/search.js
@@ -3,79 +3,76 @@ var router = express.Router();
 var url = require('url');
 var airtable = require('airtable');
 
-const db = airtable.base("app869zB8b6uHjyHS");
-
 /* GET search results */
 router.get('/', function(req, res, next) {
-	console.log(req.query);
-    res.render('search', { 
-    	title: 'CannaClarified',
-    	q: req.query.q,
-    	results: db_search(req.query.q)
-    });
+	console.log('query', req.query);
+	db_search(req.query.q, function(results) {
+		res.render('search', { 
+	    	title:   'CannaClarified',
+	    	q: 		 req.query.q,
+	    	results: results
+		});
+	});
 });
 
-function db_search(q) {
+// Airtable doesn't support querying on joined tables so working around it for now. General approach is 
+//   1) use search term (q) to look up conditions
+//   2) use condition id to look up evidence
+function db_search(q, complete) {
+	const db = airtable.base("app869zB8b6uHjyHS");
 
-/*
- *  TODO: resolve out of order async calls to conditions/evidence tables
- *
-	const conditions = db("conditions").select({filterByFormula: "OR(FIND(LOWER(\"stress\"), LOWER(Description)), FIND(LOWER(\"stress\"), LOWER(Synonyms)))"});
+	var formula = `OR(FIND(LOWER("${q}"), LOWER(Description)), FIND(LOWER("${q}"), LOWER(Synonyms)))`;
+	console.log('formula', formula);
+
+	var conditions = db("conditions").select({filterByFormula: formula});
 	var condition_ids = [];
+	var results = [];
 
 	conditions.firstPage(function(error, records) {
+		if (records == null || records.length == 0) {
+			complete(results);
+		}
+
 	    records.forEach(function(record) {
-    	    console.log('Retrieved', record.get('Description'));
+    	    console.log('condition', record.get('Description'));
     	    condition_ids.push(record.get('ID'));
     	});
-    	console.log('one', condition_ids);
-	});
 
-	console.log('two', condition_ids);
-
-	var results = [];
-	condition_ids.forEach(function(id) {
-		const evidence = db("evidence").select({filterByFormula: 'ID == ${id}'});
-		console.log('ID == ${id}');
-
-	});
-*/
-
-	var title   = "Isolation of Aspergillus caninus (Synonym: Phialosimplex caninus) from a Canine Iliac Lymph node.";
-	var authors = "Kano R, Sakai M, Hiyama M, Tani K.";
-	var journal = "Mycopathologia. 2019 Apr;184(2):335-339. doi: 10.1007/s11046-018-0312-3. Epub 2019 Jan 31.";
-	var summary = "Aspergillus caninus (synonym: Phialosimplex caninus) is an anamorphic fungus species associated with systemic infections in dogs that has been transferred from the genus Phialosimplex to Aspergillus. ...Since the response of the patient dog to ITZ and VRZ treatments …";
-
-	var results = [
-		{
-			title: title,
-			authors: authors,
-			journal: journal,
-			summary: summary
-		}, {
-			title: title,
-			authors: authors,
-			journal: journal,
-			summary: summary
-		}, {
-			title: title,
-			authors: authors,
-			journal: journal,
-			summary: summary
-		}, {
-			title: title,
-			authors: authors,
-			journal: journal,
-			summary: summary
-		}, {
-			title: title,
-			authors: authors,
-			journal: journal,
-			summary: summary
+    	console.log('condition ids', condition_ids);
+    	if (condition_ids.length == 0) {
+			throw "expected at least 1 condition id";
 		}
-	]
 
-	return results;
+    	condition_ids.forEach(function(id) {
+			const evidence = db("evidence").select({filterByFormula: `FIND(${id}, Condition)`});
+			console.log('two', `FIND(${id}, Condition)`);
+			evidence.firstPage(function(error, records) {
+				if (records == null || records.length == 0) {
+					complete(results);
+				}
+
+			    records.forEach(function(record) {
+		    	    results.push({
+		    	    	title:   record.get('Study Title'),
+						authors: record.get('First Author'),
+						journal: record.get('Journal'),
+						summary: record.get('Summary')
+		    	    });
+		    	});
+
+		    	console.log('evidence', results);
+
+		    	complete(results);
+		    });
+		});
+	});
+
+	// TODO
+	//   - error handling for empty/null results
+	//   - account for results that spill beyond "firstPage" (eg: results > 100)
+	//   - expand current assumption that only 1 condition is found by search
+	//   - expand current assumption that only conditions are being searched
+	//   - move away from airtable for simpler querying, lower latency
 }
 
 module.exports = router;
