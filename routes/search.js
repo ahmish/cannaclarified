6107194020afe2017d1fb8115004b7e2464fd741
Ahmed Abdalla <aabdalla@gmail.com>
2021-02-28 02:11:12 -0500

    adding support for dotenv & airtable

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var url = require('url');
+var airtable = require('airtable');
+
+const db = airtable.base("app869zB8b6uHjyHS");
 
 /* GET search results */
 router.get('/', function(req, res, next) {
@@ -13,6 +16,31 @@ router.get('/', function(req, res, next) {
 });
 
 function db_search(q) {
+
+/*
+ *  TODO: resolve out of order async calls to conditions/evidence tables
+ *
+	const conditions = db("conditions").select({filterByFormula: "OR(FIND(LOWER(\"stress\"), LOWER(Description)), FIND(LOWER(\"stress\"), LOWER(Synonyms)))"});
+	var condition_ids = [];
+
+	conditions.firstPage(function(error, records) {
+	    records.forEach(function(record) {
+    	    console.log('Retrieved', record.get('Description'));
+    	    condition_ids.push(record.get('ID'));
+    	});
+    	console.log('one', condition_ids);
+	});
+
+	console.log('two', condition_ids);
+
+	var results = [];
+	condition_ids.forEach(function(id) {
+		const evidence = db("evidence").select({filterByFormula: 'ID == ${id}'});
+		console.log('ID == ${id}');
+
+	});
+*/
+
 	var title   = "Isolation of Aspergillus caninus (Synonym: Phialosimplex caninus) from a Canine Iliac Lymph node.";
 	var authors = "Kano R, Sakai M, Hiyama M, Tani K.";
 	var journal = "Mycopathologia. 2019 Apr;184(2):335-339. doi: 10.1007/s11046-018-0312-3. Epub 2019 Jan 31.";
